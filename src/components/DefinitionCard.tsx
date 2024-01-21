cde99389b77618f5c07d2ca3a79ab3af5da04c1f
Joseph <jvp.2703@gmail.com>
2024-01-21 21:28:06 +0000

    revert: syn&ant tw color props not working when app deployed

--- src/components/DefinitionCard.tsx
+++ src/components/DefinitionCard.tsx
@@ -37,9 +37,9 @@ const DefinitionCard: React.FC<Props> = ({ item }) => {
           {onCopy ? "copied!" : "copy definition"}
         </button>
       </div>
-      <p className="mb-2 inline-flex rounded-sm border border-neutral-700 px-1 py-0.5 text-sm text-neutral-400">
+      <h1 className="mb-2 inline-flex rounded-sm border border-neutral-700 px-1 py-0.5 text-sm text-neutral-400">
         {item.partOfSpeech || "general"}
-      </p>
+      </h1>
       <DefWordButton text={item.definition} />
       {(item.examples || item.synonyms || item.antonyms) && (
         <details open={settings.details}>
@@ -65,13 +65,15 @@ const DefinitionCard: React.FC<Props> = ({ item }) => {
               <p className="mb-2 text-sm text-neutral-400">Synonyms</p>
               <ul className="flex flex-wrap gap-2">
                 {item.synonyms?.map((syn, idx) => (
-                  <SynAntBtn
+                  <li
+                    className="rounded-sm bg-purple-500/10 px-1.5 py-0.5 text-sm text-purple-300 duration-200 hover:text-purple-200"
                     key={idx}
-                    color={"purple"}
-                    word={syn}
-                    clean={false}
-                    fetchDictionary={fetchDictionary}
-                  />
+                  >
+                    <button onClick={() => fetchDictionary(syn, false)}>
+                      {syn}
+                    </button>
+                  </li>
+                  // <SynAntBtn key={idx} color={"purple"} syn={syn} clean={false} fetchDictionary={fetchDictionary} />
                 ))}
               </ul>
             </div>
@@ -81,13 +83,15 @@ const DefinitionCard: React.FC<Props> = ({ item }) => {
               <p className="mb-2 text-sm text-neutral-400">Antonyms</p>
               <ul className="flex flex-wrap gap-2">
                 {item.antonyms?.map((ant, idx) => (
-                  <SynAntBtn
+                  <li
+                    className="rounded-sm bg-orange-500/10 px-1.5 py-0.5 text-sm text-orange-300 duration-200 hover:text-orange-200"
                     key={idx}
-                    color={"orange"}
-                    word={ant}
-                    clean={false}
-                    fetchDictionary={fetchDictionary}
-                  />
+                  >
+                    <button onClick={() => fetchDictionary(ant, false)}>
+                      {ant}
+                    </button>
+                  </li>
+                  // <SynAntBtn key={idx} color={"purple"} syn={ant} clean={false} fetchDictionary={fetchDictionary} />
                 ))}
               </ul>
             </div>
@@ -100,22 +104,23 @@ const DefinitionCard: React.FC<Props> = ({ item }) => {
 
 export default DefinitionCard;
 
-const SynAntBtn = ({
-  color,
-  word,
-  clean,
-  fetchDictionary,
-}: {
-  color: string;
-  word: string;
-  clean: boolean;
-  fetchDictionary: (word: string, clean: boolean) => Promise<void>;
-}) => {
-  return (
-    <li
-      className={`rounded-sm bg-${color}-500/10 px-1.5 py-0.5 text-sm text-${color}-300 duration-200 hover:text-purple-200`}
-    >
-      <button onClick={() => fetchDictionary(word, clean)}>{word}</button>
-    </li>
-  );
-};
+//! 'color' tw classes not working when app is deployed
+// const SynAntBtn = ({
+//   color,
+//   syn,
+//   clean,
+//   fetchDictionary,
+// }: {
+//   color: string;
+//   syn: string;
+//   clean: boolean;
+//   fetchDictionary: (syn: string, clean: boolean) => Promise<void>;
+// }) => {
+//   return (
+//     <li
+//       className={`rounded-sm bg-${color}-500/10 px-1.5 py-0.5 text-sm text-${color}-300 duration-200 hover:text-purple-200`}
+//     >
+//       <button onClick={() => fetchDictionary(syn, clean)}>{syn}</button>
+//     </li>
+//   );
+// };
